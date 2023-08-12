23d5ce03fe8fb4a4230cba33e87201be6b3ef2c1
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2023-08-12 13:32:42 +0700

    disable masquerade to get transparent ip
    fix interface

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@
 const { release } = require('./package.json');
 
 module.exports.RELEASE = release;
+module.exports.DOCKER_SUBNET = process.env.DOCKER_SUBNET || '10.1.0.0/20';
 module.exports.PORT = process.env.PORT || 51821;
 module.exports.PASSWORD = process.env.PASSWORD;
 module.exports.WG_PATH = process.env.WG_PATH || '/etc/wireguard/';
@@ -19,11 +20,18 @@ module.exports.WG_ALLOWED_IPS = process.env.WG_ALLOWED_IPS || '0.0.0.0/0, ::/0';
 
 module.exports.WG_PRE_UP = process.env.WG_PRE_UP || '';
 module.exports.WG_POST_UP = process.env.WG_POST_UP || `
-iptables -t nat -A POSTROUTING -s ${module.exports.WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o ${module.exports.WG_DEVICE} -j MASQUERADE;
+iptables -t nat -A POSTROUTING ! -d ${module.exports.DOCKER_SUBNET} -o ${module.exports.WG_DEVICE} -j MASQUERADE;
 iptables -A INPUT -p udp -m udp --dport 51820 -j ACCEPT;
 iptables -A FORWARD -i wg0 -j ACCEPT;
 iptables -A FORWARD -o wg0 -j ACCEPT;
 `.split('\n').join(' ');
+
+// module.exports.WG_POST_UP = process.env.WG_POST_UP || `
+// iptables -t nat -A POSTROUTING -s ${module.exports.WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o ${module.exports.WG_DEVICE} -j MASQUERADE;
+// iptables -A INPUT -p udp -m udp --dport 51820 -j ACCEPT;
+// iptables -A FORWARD -i wg0 -j ACCEPT;
+// iptables -A FORWARD -o wg0 -j ACCEPT;
+// `.split('\n').join(' ');
 
 module.exports.WG_PRE_DOWN = process.env.WG_PRE_DOWN || '';
 module.exports.WG_POST_DOWN = process.env.WG_POST_DOWN || '';
